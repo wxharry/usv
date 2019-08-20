f58d263e78b913d3de0cf55326a76e0a4ea7d0e9
wxh <wxh170607@163.com>
2019-08-20 23:25:31 +0800

    Initial Commit

---
+++ pages/data/data.js
@@ -0,0 +1,201 @@
+// pages/data/data.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    battery:100,
+    map:{
+      longitude:121.40170584,
+      latitude:31.31244167
+    },
+    paras: [
+      {
+        name: "电导率",
+        data: 321,
+        status: "正常"
+      },
+      {
+        name: "溶解氧",
+        data: 13.74,
+        status: "正常"
+      },
+      {
+        name: "ORP",
+        data: 138,
+        status: "正常"
+      },
+      {
+        name: "氨氮",
+        data: 0.8,
+        status: "正常"
+      },
+      {
+        name: "浊度",
+        data: 14.9,
+        status: "正常"
+      },
+      {
+        name: "pH",
+        data: 9.17,
+        status: "正常"
+      },
+      {
+        name: "温度",
+        data: 23.8,
+        status: "正常"
+      },
+      {
+        name: "速度",
+        data: 1.0,
+      }],
+    markers: [{
+      iconPath: "/images/icon.png",
+      id: 0,
+      latitude: 31.31244167,
+      longitude: 121.40170584,
+      // latitude:31.31617737
+      // longitude:121.38876343
+      width: 30,
+      height: 30
+    }],
+    // polyline: [{
+    //   points: [{
+    //     longitude: 31.31244167,
+    //     latitude: 121.40170584
+    //   }, {
+    //     longitude: 31.32244180,
+    //     latitude: 121.42170590
+    //   }],
+    //   color: "#000000",
+    //   width: 2,
+    //   dottedLine: true
+    // }],
+    controls: [{
+      id: 1,
+      // iconPath: '/images/icon.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+
+  },
+
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    var that = this
+
+    setTimeout(function () {
+      that.setData({
+        "markers[0].longitude": that.data.markers[0].longitude + 0.000001,
+        "markers[0].latitude": that.data.markers[0].latitude,
+        "map.longitude": that.data.map.longitude + 0.000001,
+        "map.latitude": that.data.map.latitude
+      })//要延时执行的代码
+      console.log(that.data.markers[0].longitude)
+    }, 1500) //延迟时间 这里是1秒
+
+    /*wx.request({ //需要实时更新把他打开，测试用就没打开
+      url: 'http://172.20.10.5:8000/show_data/',
+      data: '',
+      header: {},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        if (res.statusCode == 200) {
+          console.log("success")
+          console.log(res)
+          var data = res.data
+          that.setData({
+            battery: data.battery,
+            "paras[0].data": data.diandaolv,
+            "paras[1].data": data.rongjieyang,
+            "paras[2].data": data.orp,
+            "paras[3].data": data.andan,
+            "paras[4].data": data.zhuodu,
+            "paras[5].data": data.ph,
+            "paras[6].data": data.temperature,
+            "paras[7].data": data.vel,
+            "markers[0].longitude": data.longitude,
+            "markers[0].latitude": data.latitude
+          })
+        }
+        else {
+          console.log(res.statusCode)
+        }
+      },
+      fail: function (res) {
+        console.log("fail")
+      },
+      complete: function (res) {
+        
+      },
+    })*/
+    this.onReady()
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
---
+++ pages/index/index.js
@@ -0,0 +1,87 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    start: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+  },
+  tapbtn:function(){
+    var flag = this.data.start
+    this.setData({
+      start: !flag
+    })
+    wx.navigateTo({
+      url: '/pages/data/data',
+    })
+    // wx.request({
+    //   url: 'http://172.20.10.5:8000/simulate/',
+    //   data: '',
+    //   header: {},
+    //   method: 'GET',
+    //   dataType: 'json',
+    //   responseType: 'text',
+    //   success: function(res) {
+    //     console.log("success")
+    //     wx.navigateTo({
+    //       url: '/pages/data/data',
+    //     })
+    //   },
+    //   fail: function(res) {
+    //     console.log("fail")
+    //   },
+    //   complete: function(res) {},
+    // })
+
+  },
+
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+})
